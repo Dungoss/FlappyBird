b15b09e93b81f1e60d483e7154556122924f02a2
Lê Duy Dũng <70448211+Dungoss@users.noreply.github.com>
2024-02-29 11:02:31 +0700

    death when fall off screen, add point audio

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -8,15 +8,37 @@ public class LogicScript : MonoBehaviour {
     public int playerScore;
     public Text scoreText;
     [SerializeField] GameObject gameOverScene;
+    public bool canAddScore = true;
+
+    public AudioClip audioClip; // Declare it as AudioClip, not AudioSource
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        // Assign the AudioClip to the AudioSource
+        audioSource.clip = audioClip;
+
+        // Play the audio
+
+    }
+
 
     public void AddScore(int scoreToAdd)
     {
-        playerScore += scoreToAdd;
-        scoreText.text = playerScore.ToString();
+        if(canAddScore)
+        {
+            playerScore += scoreToAdd;
+            scoreText.text = playerScore.ToString();
+            audioSource.Play();
+        }
     }
 
     public void GameOver()
     {
+        canAddScore = false;
         gameOverScene.SetActive(true);
     }
 
